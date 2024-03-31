90a57d758677f3f8658ce474b06ceff3d7ed49de
Jayaharini muthukkandan <146460741+Jayaharinimuthukkandan@users.noreply.github.com>
2024-03-31 09:50:38 +0530

    Update script.js
    changes

--- script.js
+++ script.js
@@ -1,69 +1,81 @@
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainer = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-let shuffledQuestions, currentQuestionIndex;
-startButton.addEventListener('click', startGame);
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++;
-  setNextQuestion();
-});
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
 function startGame() {
-  startButton.classList.add('hide');
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5).slice(0, 50); // Selecting first 50 shuffled questions
-  currentQuestionIndex = 0;
-  questionContainer.classList.remove('hide');
-  setNextQuestion();
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5).slice(0,50)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
+
 function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
 function showQuestion(question) {
-  questionElement.innerText = question.question;
+  questionElement.innerText = question.question
   question.answers.forEach(answer => {
-    const button = document.createElement('button');
-    button.innerText = answer.text;
-    button.classList.add('btn');
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
     if (answer.correct) {
-      button.dataset.correct = answer.correct;
+      button.dataset.correct = answer.correct
     }
-    button.addEventListener('click', selectAnswer);
-    answerButtonsElement.appendChild(button);
-  });
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
+
 function resetState() {
-  nextButton.classList.add('hide');
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
 }
+
 function selectAnswer(e) {
-  const selectedButton = e.target;
-  const correct = selectedButton.dataset.correct;
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct);
-  });
+    setStatusClass(button, button.dataset.correct)
+  })
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide');
+    nextButton.classList.remove('hide')
   } else {
-    startButton.innerText = 'Restart';
-    startButton.classList.remove('hide');
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
   }
 }
+
 function setStatusClass(element, correct) {
-  clearStatusClass(element);
+  clearStatusClass(element)
   if (correct) {
-    element.classList.add('correct');
+    element.classList.add('correct')
   } else {
-    element.classList.add('wrong');
+    element.classList.add('wrong')
   }
 }
+
 function clearStatusClass(element) {
-  element.classList.remove('correct');
-  element.classList.remove('wrong');
+  element.classList.remove('correct')
+  element.classList.remove('wrong' )
 }
+
 const questions = [
   {
     question: 'What is 2 + 2?',
